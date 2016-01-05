f26cfa4e69879b0e50eabae34688e8a6642fe928
penyuanliao <anaylst@gmail.com>
2016-01-05 16:52:32 +0800

    修改logger紀錄,增加紀錄ip port 狀態

--- FMSServerLog/fxNetSocket/lib/FxLogger.js
+++ FMSServerLog/fxNetSocket/lib/FxLogger.js
@@ -7,7 +7,9 @@ var fs = require('fs'),
     exec = require('child_process'),
     log_file = fs.createWriteStream(__dirname + '/'+ formatDate() +'.log',{ flags:'w' });
 var logger =  function logger() {
+    /* Variables */
 
+    /* Codes */
 };
 
 logger.prototype.debug = function (d) {
@@ -16,23 +18,45 @@ logger.prototype.debug = function (d) {
     log_file.write(st + util.format(d) + '\r\n'); // win:\r\n linux:\n mac:\r
     //console.log(st, util.format(d));
 };
-
+/**
+ * polling set timer run child process state.
+ * @param proc : child_process
+ * @param name : link name
+ * @param delay : delay
+ */
 logger.prototype.pollingWithProcess = function(proc, name, delay) {
-    setInterval(function () {
-
-        console.log('loop is running', typeof proc != 'undefined', proc !== null, proc !== "");
+    var keepWatch = setInterval(function () {
+        if (proc.running == false && proc.STATUS >= 2) {
+            setTimeout(function () {
+                clearInterval(keepWatch);
+            },delay*2);
+        }
         exec.exec('ps -p ' + proc.pid + ' -o rss,pmem,pcpu,vsize,time',function (err, stdout, stderr) {
             err = err || stderr;
             if (!err) {
-                logger.instance.debug('[sysInfo] ffmpeg'+ name + '\r\n' + stdout.toString());
-                logger.instance.debug('[Nodejs]process.memoryUsage: ' + process.memoryUsage().toString());
+                logger.instance.debug('[SYSINFO] ffmpeg'+ name + '\r\n' + stdout.toString());
+                logger.instance.debug('[Nodejs]process.memoryUsage: rss=' + process.memoryUsage().rss + ", heapTotal=" + process.memoryUsage().heapTotal + ", heapUsed=" + process.memoryUsage().heapUsed);
             };
         });
+        /** 檢查各種狀態 **/
+        if (typeof proc != 'undefined' && (proc !== null) && proc !== "") {
 
-        if (typeof proc != 'undefined' && (proc !== null) && proc !== "")
-            logger.instance.debug("[Polling] ffmpeg " + name + " process to Working.");
-        else
-            logger.instance.debug("[Polling] ffmpeg " + name + " process to Shutdown.");
+            if (parseInt(proc.exitCode) === 255) {
+                console.log("[Polling] ffmpeg " + name + " process to Shutdown. (use kill PID) -proc.exitCode=" + proc.exitCode + " -proc.killed=" + proc.killed + " -proc.signalCode=" + proc.signalCode);
+                logger.instance.debug("[Polling] ffmpeg " + name + " process to Shutdown. (use kill -15 PID) -proc.exitCode=" + proc.exitCode + " -proc.killed=" + proc.killed + " -proc.signalCode=" + proc.signalCode);
+            }else if (proc.signalCode === "SIGKILL") {
+                console.log("[Polling] ffmpeg " + name + " process to Shutdown. (use kill -9 PID) -proc.exitCode=" + proc.exitCode + " -proc.killed=" + proc.killed + " -proc.signalCode=" + proc.signalCode);
+                logger.instance.debug("[Polling] ffmpeg " + name + " process to Shutdown. (use kill -9 PID) -proc.exitCode=" + proc.exitCode + " -proc.killed=" + proc.killed + " -proc.signalCode=" + proc.signalCode);
+            }else {
+                console.log("[Polling] ffmpeg " + name + " process to Working." + " -proc.exitCode=" + proc.exitCode + " -proc.killed=" + proc.killed + " -proc.signalCode=" + proc.signalCode);
+            }
+
+        }
+        else{
+            logger.instance.debug("[Polling] ffmpeg " + name + " process is NULL.");
+        }
+
+
     },delay);
 };
 
@@ -40,13 +64,34 @@ function formatDate() {
     var date = new Date();
     return (date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate());
 };
+/** ping once ipAddress confirm network has connection. **/
+logger.prototype.reachabilityWithHostName = function (name) {
+
+    var args = name.toString().split(":");
+
+    var nc = exec.exec("nc -vz " + args[0] + " " + args[1], function (err, stdout, stderr) {
+       err = err || stderr;
+
+        console.log(new Date(),"to ",name,":",stdout.toString().search("succeeded!"));
+        logger.instance.debug("reachability:" + stdout);
+    });
+};
+
 
 logger.prototype.logTotalMemoryUsage = function (PIDs) {
     exec.exec("ps -p " + PIDs + " -o pcpu,pmem,vsz,rss | awk '{pcpu += $1; pmem += $2; vsz += $3; rss += $4;} END { print pcpu, pmem, vsz, rss }'", function (err, stdout, stderr) {
         err = err || stderr;
         if (!err) {
             var args = stdout.toString().split(" ");
-            console.log("%CPU=" + args[0] + ",%MEM=" + args[1] + ",VSZ=" + args[2] + ",RSS=" + args[3]);
+            console.log(new Date(),">> Total Memory %CPU=" + args[0] + ",%MEM=" + args[1] + ",VSZ=" + args[2] + ",RSS=" + args[3]);
+        }
+    });
+};
+logger.prototype.procState = function (pid,callback) {
+    exec.exec("ps -p " + pid + " -o pid | awk '{pid += $1;} END {print pid}'", function (err, stdout, stderr) {
+        err = err || stderr;
+        if (!err) {
+            callback((pid === stdout))
         }
     });
 };
